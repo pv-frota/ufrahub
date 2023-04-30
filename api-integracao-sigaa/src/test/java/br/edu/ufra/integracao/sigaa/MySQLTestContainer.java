13fae79c9d48d77dc2fb4586ea567fd6d62e4829
pedro.victor <pvjacinto8@gmail.com>
2023-04-30 13:34:17 -0300

    Fix: refatorando MySQLTestContainer e dev.properties

--- api-integracao-sigaa/src/test/java/br/edu/ufra/integracao/sigaa/MySQLTestContainer.java
+++ api-integracao-sigaa/src/test/java/br/edu/ufra/integracao/sigaa/MySQLTestContainer.java
@@ -3,6 +3,7 @@ package br.edu.ufra.integracao.sigaa;
 import org.flywaydb.core.Flyway;
 import org.junit.jupiter.api.BeforeAll;
 import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.MySQLContainer;
@@ -12,19 +13,20 @@ import org.testcontainers.utility.DockerImageName;
 
 @Testcontainers
 @DirtiesContext
-public class AbstractTestContainer {
+@ActiveProfiles("dev")
+public class MySQLTestContainer {
+
+    private static final DockerImageName image = DockerImageName.parse("mysql:8.0.22");
+
     @Container
-    private static final MySQLContainer container =
-            new MySQLContainer(DockerImageName.parse("mysql"))
-                    .withDatabaseName("mydb")
-                    .withUsername("root")
-                    .withPassword("root");
+    private static final MySQLContainer<?> container = new MySQLContainer<>(image);
 
     @DynamicPropertySource
     private static void properties(DynamicPropertyRegistry registry) {
         registry.add("spring.datasource.url", container::getJdbcUrl);
         registry.add("spring.datasource.username", container::getUsername);
         registry.add("spring.datasource.password", container::getPassword);
+        registry.add("spring.flyway.locations", () -> "classpath:db/migrations/test");
     }
 
     @BeforeAll
